ff2b0c48c2db332a7fe41d350bb4722148c32446
kongesque <kongesque@gmail.com>
2026-04-10 15:53:49 +0000

    refactor: move unused files to archive/, drop jailbreakv_28k from datasets package

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,13 +1,11 @@
 from .mmsafety import UnsafeVLMDataset_MMsafety
 from .figstep import UnsafeVLMDataset_fig_step
-from .jailbreakv_28k import UnsafeVLMDataset_28k
 from .jailbreakv_nano import UnsafeVLMDataset_jailbreakv_nano
 from .mmvet import VLMDataset_mmvet
 
 __all__ = [
     "UnsafeVLMDataset_MMsafety",
     "UnsafeVLMDataset_fig_step",
-    "UnsafeVLMDataset_28k",
     "UnsafeVLMDataset_jailbreakv_nano",
     "VLMDataset_mmvet",
 ]
